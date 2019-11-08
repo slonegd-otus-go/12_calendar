b0d3f36b0ba41131937cfc52f82e48a57f78e051
slonegd <slonkdv@mail.ru>
2019-11-08 09:32:13 +0300

    realize scanner

--- internal/event/event.go
+++ internal/event/event.go
@@ -26,8 +26,53 @@ func StartScan(storage Storage, onEvent func(Event)) {
 
 func scan(storage Storage, onEvent func(Event)) {
 	ticker := time.NewTicker(1 * time.Second)
+	publishedIDs := make(IDset)
 	for range ticker.C {
-		event := Event{} // TODO определить Event
-		onEvent(event)
+		events := storage.Active(time.Now())
+		ids := makeIDset(events)
+		publishedIDs = intersection(publishedIDs, ids)
+		for id, event := range events {
+			if publishedIDs.contains(id) {
+				continue
+			}
+			onEvent(event)
+			publishedIDs.add(id)
+		}
 	}
+}
+
+type IDset map[ID]struct{}
+
+func makeIDset(ids map[ID]Event) IDset {
+	result := make(IDset)
+	for id := range ids {
+		result.add(id)
+	}
+	return result
+}
+
+func (set IDset) contains(id ID) bool {
+	_, ok := set[id]
+	return ok
+}
+
+func (set IDset) add(id ID) {
+	set[id] = struct{}{}
+}
+
+func (set IDset) complement(other IDset) {
+	for v := range other {
+		delete(set, v)
+	}
+}
+
+// https://en.wikipedia.org/wiki/Intersection_(set_theory)
+func intersection(ids1, ids2 IDset) IDset {
+	result := make(IDset)
+	for id := range ids1 {
+		if ids2.contains(id) {
+			result.add(id)
+		}
+	}
+	return result
 }
