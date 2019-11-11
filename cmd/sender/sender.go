b322c31841c540aefb1b4191b07b9e420a7ae546
slonegd <slonkdv@mail.ru>
2019-11-11 16:11:40 +0300

    add sender to docker compose

--- cmd/sender/sender.go
+++ cmd/sender/sender.go
@@ -6,16 +6,16 @@ import (
 	"github.com/slonegd-otus-go/12_calendar/internal/event/amqpsubscriber"
 )
 
-var ampqURL string
+var amqpURL string
 
 func init() {
-	Command.Flags().StringVar(&ampqURL, "ampqurl", "amqp://guest:guest@localhost:5672", "url to ampq server")
+	Command.Flags().StringVar(&amqpURL, "amqpurl", "amqp://guest:guest@localhost:5672", "url to amqp server")
 }
 
 var Command = &cobra.Command{
 	Use:   "sender",
 	Short: "Run event sender (amqp subscriber)",
 	Run: func(cmd *cobra.Command, args []string) {
-		amqpsubscriber.Run(ampqURL)
+		amqpsubscriber.Run(amqpURL)
 	},
 }
